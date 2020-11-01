a0bb230a3f10177b0385286823be2ad3a5403323
hnifmaghfur <hnifmaghfur@gmail.com>
2020-11-01 13:48:44 +0700

    add promise.

--- src/model/User.js
+++ src/model/User.js
@@ -12,34 +12,36 @@ module.exports = {
     });
   },
   changePassword: (id, password, newPassword) => {
-    db.query("SELECT password FROM user WHERE id=?", id, (err, result) => {
-      // console.log(result[0].password);
-      if (!err) {
-        bcrypt.compare(password, result[0].password, (err, result) => {
-          if (result) {
-            bcrypt.hash(newPassword, 10, (err, hashNewPassword) => {
-              if (!err) {
-                db.query(
-                  "UPDATE user SET password=? WHERE id=? ",
-                  hashNewPassword,
-                  id,
-                  (err, result) => {
-                    if (!err) {
-                      resolve(result);
-                    } else {
-                      return reject(err);
+    return new Promise((resolve, reject) => {
+      db.query("SELECT password FROM user WHERE id=?", id, (err, result) => {
+        console.log(result[0].password);
+        if (!err) {
+          bcrypt.compare(password, result[0].password, (err, result) => {
+            if (result) {
+              bcrypt.hash(newPassword, 10, (err, hashNewPassword) => {
+                if (!err) {
+                  db.query(
+                    "UPDATE user SET password=? WHERE id=? ",
+                    hashNewPassword,
+                    id,
+                    (err, result) => {
+                      if (!err) {
+                        resolve(result);
+                      } else {
+                        return reject(err);
+                      }
                     }
-                  }
-                );
-              } else {
-                return reject(err);
-              }
-            });
-          }
-        });
-      } else {
-        return reject(err);
-      }
+                  );
+                } else {
+                  return reject(err);
+                }
+              });
+            }
+          });
+        } else {
+          return reject(err);
+        }
+      });
     });
   },
   changePin: (id, newPin) => {
