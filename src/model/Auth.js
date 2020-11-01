64d3a766843b2a6958e9193c821651e41237c325
hnifmaghfur <hnifmaghfur@gmail.com>
2020-11-01 20:52:10 +0700

    update isActive

--- src/model/Auth.js
+++ src/model/Auth.js
@@ -1,6 +1,7 @@
 const db = require("../helper/db");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const formResponse = require("../helper/formResponse");
 
 module.exports = {
   register: (email, password, fullName) => {
@@ -23,47 +24,64 @@ module.exports = {
   },
 
   login: (email, password) => {
-    console.log(email, password, "model");
+    // console.log(email, password, "model");
     return new Promise((resolve, reject) => {
-      db.query("SELECT * FROM user WHERE email= ?", email, (err, res) => {
-        if (!err) {
-          let data = [];
-          if (res.length > 0) {
-            data = res;
-          } else {
-            data = [
-              {
-                id: 0,
-                name: "kosong",
-                email: "kosong",
-                roles: "kosong",
-                password:
-                  "$2y$10$4W/5o6dcvCIjm9ym/dkXQ.l6C/p8rCh0swxh8NlvwvAWfYG0gLSUO ",
-              },
-            ];
-          }
+      db.query(
+        "SELECT isActive FROM user WHERE email= ?",
+        email,
+        (err, res) => {
+          // console.log(res[0].isActive);
+          if (res[0].isActive == "1") {
+            db.query("SELECT * FROM user WHERE email= ?", email, (err, res) => {
+              if (!err) {
+                let data = [];
+                if (res.length > 0) {
+                  data = res;
+                } else {
+                  data = [
+                    {
+                      id: 0,
+                      name: "kosong",
+                      email: "kosong",
+                      roles: "kosong",
+                      password:
+                        "$2y$10$4W/5o6dcvCIjm9ym/dkXQ.l6C/p8rCh0swxh8NlvwvAWfYG0gLSUO ",
+                    },
+                  ];
+                }
 
-          const { password: hashedPassword, roleId, email, id, name } = data[0];
-          console.log(hashedPassword);
-          bcrypt.compare(password, hashedPassword, (error, result) => {
-            console.log(result);
-            if (result) {
-              const tokenJWT = jwt.sign(
-                { email, id, name, roleId },
-                process.env.SECRET_KEY
-              );
-              const token = `Bearer ${tokenJWT}`;
-              const data = {
-                token: token,
-                role: roleId,
-              };
-              resolve(data);
-            } else {
-              return reject(error);
-            }
-          });
+                const {
+                  password: hashedPassword,
+                  roleId,
+                  email,
+                  id,
+                  name,
+                } = data[0];
+                console.log(hashedPassword);
+                bcrypt.compare(password, hashedPassword, (error, result) => {
+                  console.log(result);
+                  if (result) {
+                    const tokenJWT = jwt.sign(
+                      { email, id, name, roleId },
+                      process.env.SECRET_KEY
+                    );
+                    const token = `Bearer ${tokenJWT}`;
+                    const data = {
+                      token: token,
+                      role: roleId,
+                    };
+                    resolve(data);
+                  } else {
+                    return reject(error);
+                  }
+                });
+              }
+            });
+          } else {
+            reject(err);
+          }
         }
-      });
+      );
     });
   },
 
